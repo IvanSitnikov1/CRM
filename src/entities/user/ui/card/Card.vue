678682b10c0da12b0717010432c1a65ca21f42b9
Сергей С. Зябликов <sergey.zyablikov@arealidea.ru>
2024-01-30 17:37:59 +0300

    fetch: Добавил CircleProgressBar.vue

--- src/entities/user/ui/card/Card.vue
+++ src/entities/user/ui/card/Card.vue
@@ -1,11 +1,18 @@
 <template>
   <article class="employer-card" v-for="index in 2" :key="index">
-    <div class="employer-card__avatar">
-      <img class="employer-card__image" :src="image" alt="avatar">
-    </div>
-    <div class="header header_size-small">
-      Shawn Stone
-    </div>
+    <CircleProgressBar class="employer-card__circle" :data="{
+      size: 64,
+      value: 23,
+      max: 66,
+    }">
+      <div class="employer-card__avatar" >
+        <img class="employer-card__image" :src="image" alt="avatar">
+      </div>
+    </CircleProgressBar>
+    <Title :data="{
+      size: 'small',
+      title: 'Shawn Stone'
+    }"/>
     <div class="employer-card__job">
       UI/UX Designer
     </div>
@@ -17,6 +24,8 @@
 
 <script setup lang="ts">
 import { type IBook } from 'src/entities/user'
+import { CircleProgressBar } from '@/shared/ui/circle-progress-bar'
+import { Title } from '@/shared/ui/title'
 
 const props = defineProps<{
   data: IBook
