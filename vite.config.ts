253c916d7f706de7df5a61ef904de6e22e2dd3b2
Сергей С. Зябликов <sergey.zyablikov@arealidea.ru>
2024-02-15 16:32:28 +0300

    fetch: change icon

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,18 @@ const manifestForPWA = {
       purpose: 'favicon'
     },
     {
+      src: './favicon-16x16.png',
+      sizes: '16x16',
+      type: 'image/png',
+      purpose: 'favicon'
+    },
+    {
+      src: './favicon-32x32.png',
+      sizes: '32x32',
+      type: 'image/png',
+      purpose: 'favicon'
+    },
+    {
       src: './apple-touch-icon.png',
       sizes: '180x180',
       type: 'image/png',
