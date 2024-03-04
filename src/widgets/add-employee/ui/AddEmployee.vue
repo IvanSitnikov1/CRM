76d89aa35fb386a56fe76a98538b9620db3a9ed3
Сергей С. Зябликов <sergey.zyablikov@arealidea.ru>
2024-03-04 16:19:56 +0300

    fix: add employeee

--- src/widgets/add-employee/ui/AddEmployee.vue
+++ src/widgets/add-employee/ui/AddEmployee.vue
@@ -1,48 +1,65 @@
 <template>
-  <PopUp @onClose="closeAddEmployee"
-         @onOpen="openAddEmployee"
-         :data="{
+    <PopUp
+        @onClose="closeAddEmployee"
+        @onOpen="openAddEmployee"
+        :data="{
             title: 'Add Employee?',
-            open: state.showAddEmployee
-          }">
-    <template #content>
-      <form class="support">
-        <UISelect :data="{
-          text: 'Member’s Email',
-          isMultipleSelect: true
-        }"/>
-        <UiButton :data="{
-          title: 'Approve',
-          type: 'submit'
-        }"/>
-      </form>
-    </template>
-  </PopUp>
+            open: state.showAddEmployee,
+        }"
+    >
+        <template #content>
+                <div class="add-employee__list-member">
+                  <UISelect
+                    :data="{
+                        text: '',
+                    }"
+                  />
+                </div>
+                <Link
+                  class="add-employee__add"
+                  :data="{
+                  title: 'Add another Member',
+                  to: '/',
+                  iconLeft: 'addEmployee'
+                }"/>
+        </template>
+        <template #bottom>
+          <div class="add-employee__approve">
+            <UiButton
+              :data="{
+              title: 'Approve',
+              type: 'submit',
+              isFull: true
+            }"
+            />
+          </div>
+
+        </template>
+    </PopUp>
 </template>
 
 <script setup lang="ts">
-import { PopUp } from '@/entities/popup'
-import { UiButton } from '@/shared/ui/button'
-import { UISelect } from "@/shared/ui/select";
-import { lock, unlock } from "@/shared/lib/ustils/isBlockScroll";
-import { useModalStore } from '@/entities/add-modal'
-import { storeToRefs } from 'pinia'
+import { storeToRefs } from 'pinia';
+import { PopUp } from '@/entities/popup';
+import { UiButton } from '@/shared/ui/button';
+import { UISelect } from '@/shared/ui/select';
+import { lock, unlock } from '@/shared/lib/ustils/isBlockScroll';
+import { useModalStore } from '@/entities/add-modal';
+import { Link } from '@/shared/ui/link'
 
 const modalStore = useModalStore();
 
 const { state } = storeToRefs(modalStore);
 const closeAddEmployee = () => {
-  state.value.showAddEmployee = false
-  unlock()
+  state.value.showAddEmployee = false;
+  unlock();
 };
 const openAddEmployee = () => {
-  lock()
+  lock();
 };
 </script>
 
-
 <style lang="scss">
   @import "style.module";
-
 
 </style>
