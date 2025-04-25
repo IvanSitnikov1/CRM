0cd1115710271525ba49cd2e9b919376ee02236d
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2025-04-25 10:02:17 +0300

    init

---
+++ backend/api/configs/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import declarative_base
+
+from api.configs.settings import settings
+
+
+Base = declarative_base(cls=AsyncAttrs)
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+def connection(method):
+    async def wrapper(*args, **kwargs):
+        async with async_session_maker() as session:
+            try:
+                return await method(*args, session=session, **kwargs)
+            except Exception as e:
+                await session.rollback()  # Откат транзакции при ошибке
+                raise e
+            finally:
+                await session.close()   # Закрываем сессию
+    return wrapper
--- src/app/providers/router/routes.ts
+++ src/app/providers/router/routes.ts
@@ -3,53 +3,53 @@ export const routes = [
     path: '/',
     name: 'home',
     component: () => import('@/pages/home'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/nears-events',
     component: () => import('@/pages/nearest-events'),
     name: 'nears-events',
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/calendar',
     component: () => import('@/pages/calendar'),
     name: 'calendar',
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/projects',
     component: () => import('@/pages/progects'),
     name: 'projects',
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/projects/details',
     component: () => import('@/pages/project-details'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/projects/task',
     component: () => import('@/pages/project-task'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/vacations',
     component: () => import('@/pages/vacations'),
     name: 'vacations',
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/employees',
     name: 'employees',
     component: () => import('@/pages/employees'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/messenger',
     name: 'messenger',
     component: () => import('@/pages/messenger'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/login',
@@ -59,17 +59,17 @@ export const routes = [
     path: '/profile',
     name: 'profile',
     component: () => import('@/pages/profile'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/my-profile',
     name: 'my-profile',
     component: () => import('@/pages/profile'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   },
   {
     path: '/:pathMatch(.*)',
     component: () => import('@/pages/not-found'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: false }
   }
 ];
